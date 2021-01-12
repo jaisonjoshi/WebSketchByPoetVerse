78d0e94dd33c0645ff35ccb92769e13d8937260a
iamAravinks <aravindkadaikattu@gmail.com>
2021-01-12 22:51:51 +0530

    Router, Contact us and carousel added

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
-import './Home.css';
-import logo from './Logo.png';
-
+import React from "react";
+import { Jumbotron, Container } from "reactstrap";
+import "./Home.css";
+import logo from "./Logo.png";
+import AboutSection from "../About/aboutSection";
+import Services from "../Services/Services";
+import Footer from "../Footer/Footer";
+import Client from "../testimonials/Client";
 function Home() {
-    return (
-       <>
-        <div className="hero">
-            <Jumbotron fluid className="main d-flex align-items-center text-center">
-                <Container fluid className="mx-1">
-                    <img src={logo} className="logo" /><h1 className="display-4 d-inline">epoch zero</h1>
-                    <p className="lead ">All starts from here</p>
-                </Container>
-            </Jumbotron>
+  return (
+    <>
+      <div className='hero'>
+        <Jumbotron fluid className='main d-flex align-items-center text-center'>
+          <Container fluid className='mx-1'>
+            <img src={logo} className='logo' alt='Epoch Zero' />
+            <h1 className='display-4 d-inline'>epoch zero</h1>
+            <p className='lead '>All starts from here</p>
+          </Container>
+        </Jumbotron>
+        <AboutSection />
+        <Services />
+        <div>
+          <Client />
         </div>
-       
-       </>
-    )
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
